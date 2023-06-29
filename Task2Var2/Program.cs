4136f9df531f2b7aa03b60029a6f613a5077929c
Dmitriy <trayan@yandex.ru>
2023-06-29 18:03:59 +1100

    Task2Var2 added

---
+++ Task2Var2/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ");
+    int[] res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+bool FindElement(int[] array, int el)
+{
+    foreach (int item in array)
+    {
+        if (el == item) return true;
+    }
+    return false;
+}
+Console.Clear();
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (FindElement(baseArray, n))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
